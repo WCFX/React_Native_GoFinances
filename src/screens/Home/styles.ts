6339cff1cbbbfd592791b9c90bb71ab692890c5e
wcfx <wcfx.dev@gmail.com>
2021-07-31 17:37:05 -0300

    chore: theme provider com tipagem

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,12 +3,13 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.primary};
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
   font-size: 32px;
   text-align: center;
   font-family: ${({ theme }) => theme.fonts.Montserrat700};
-  color: ${({ theme }) => theme.colors.shape};
+  color: ${({ theme }) => theme.colors.title};
 `;
