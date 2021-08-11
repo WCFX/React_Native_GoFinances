946cd0577958e758d8bac330f68f362168d4a032
wcfx <wcfx.dev@gmail.com>
2021-08-11 16:35:09 -0300

    style: tabbar with styled completed

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,17 +1,63 @@
 import React from 'react';
+import { Platform } from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { useTheme } from 'styled-components';
 
 import * as View from '../screens/index';
 
 const { Screen, Navigator } = createBottomTabNavigator();
 
-const Routes: React.FC = () => (
-  <Navigator initialRouteName="Home">
-    <Screen name="Home" component={View.Home} />
-    <Screen name="Register" component={View.Register} />
-    <Screen name="CategorySelect" component={View.CategorySelect} />
-  </Navigator>
-);
+const Routes: React.FC = () => {
+  const theme = useTheme();
+
+  return (
+    <Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        activeTintColor: theme.colors.secondary,
+        inactiveTintColor: theme.colors.title,
+        labelPosition: 'beside-icon',
+        style: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 60,
+        },
+      }}
+    >
+      <Screen
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+        name="Home"
+        component={View.Home}
+      />
+      <Screen
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="monetization-on" size={size} color={color} />
+          ),
+        }}
+        name="Registros"
+        component={View.Register}
+      />
+      <Screen
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="insights" size={size} color={color} />
+          ),
+        }}
+        name="Resumo"
+        component={View.Register}
+      />
+    </Navigator>
+  );
+};
 
 export default Routes;
